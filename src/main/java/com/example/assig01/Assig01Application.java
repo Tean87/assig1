6fdaac120e62745f60be151ca4f4ad63d834a647
tean <teancsser@gmail.com>
2025-12-21 22:31:00 +0600

    init commit

---
+++ src/main/java/com/example/assig01/Assig01Application.java
@@ -0,0 +1,13 @@
+package com.example.assig01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assig01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assig01Application.class, args);
+    }
+
+}
