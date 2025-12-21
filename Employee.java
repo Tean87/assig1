59f7cc09966c60835b1f9dc8bb17e60e56d4745a
Tean87 <teancsser@gmail.com>
2025-12-21 22:53:24 +0600

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,36 @@
+public class Employee extends Human {
+
+    private String company;
+    private float salary;
+
+    public static String employeeType = "Office worker";
+
+    public Employee(int age, String name, boolean isMale, String company, float salary) {
+        super(age, name, isMale); // получаем данные из Human
+        this.company = company;
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public void showStatus() {
+        System.out.println(
+                "I work at " + company + " and get this much money (i love money) " + salary
+        );
+    }
+}
